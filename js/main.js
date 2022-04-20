e461331fb0657d408b40052b92adbd1feedd3bd7
danjkim21 <dan.jkim21@gmail.com>
2022-04-20 12:52:33 -0400

    first commit, adding project files

---
+++ js/main.js
@@ -0,0 +1,159 @@
+// ---- Date/time display ----
+// Get current time and format
+function getTime() {
+    let date = new Date(),
+        min = date.getMinutes(),
+        //sec = date.getSeconds(),
+        hour = date.getHours();
+    return "" +
+        (hour < 10 ? ("0" + hour) : hour) + ":" +
+        (min < 10 ? ("0" + min) : min);// + ":" + 
+        //(sec < 10 ? ("0" + sec) : sec);
+}
+function getDate() {
+    let date = new Date(),
+        months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+        cmonth = months[date.getMonth()],
+        days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+        cday = days[date.getDay()],
+        cnum = date.getDate();
+    return "" + cday + ", " + cmonth + " " + cnum;
+}
+// Set up the clock and date
+document.querySelector(".timeInsert1").innerHTML = getTime();
+document.querySelector(".timeInsert2").innerHTML = getTime();
+
+document.querySelector(".dateInsert1").innerHTML = getDate();
+document.querySelector(".dateInsert2").innerHTML = getDate();
+// Set clock interval to tick clock
+setInterval(() => {
+    document.querySelector(".timeInsert1").innerHTML = getTime();
+    document.querySelector(".timeInsert2").innerHTML = getTime();
+}, 1000);
+setInterval(() => {
+    document.querySelector(".dateInsert1").innerHTML = getDate();
+    document.querySelector(".dateInsert2").innerHTML = getDate();
+
+}, 100000);
+
+
+// ---- Set weather temp and icon ----
+window.onload = () => {
+    let xhr = new XMLHttpRequest();
+    // Request to open weather map
+    xhr.open('GET', 'https://api.openweathermap.org/data/2.5/weather?id=4140963&units=imperial&appid=d2e4d541802b524195b9b8f1544ef756');
+    xhr.onload = () => {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                var prefix = "wi-";
+                var date = new Date();
+                var hour = date.getHours();
+                var dn = "";
+                if (hour > 6 && hour < 20) {
+                    dn = "";
+                } else {
+                    dn = "night-";
+                }
+                let json = JSON.parse(xhr.responseText);
+                // console.log(json);
+                var code = json.weather[0].id;
+                wIcon = prefix + "owm-" + dn + code;
+                // console.log(wIcon);
+                document.getElementById("temp").innerHTML = json.main.temp.toFixed(0) + "°";
+                document.getElementById("weather-icon").classList.add(wIcon);
+            } else {
+                console.log('error msg: ' + xhr.status);
+            }
+        }
+    }
+    xhr.send();
+}
+
+
+// ---- Ipad OS cursor test - Credits to Ralph J. Smit! https://ralphjsmit.com/animated-custom-cursor/ ----
+var cursor =  document.getElementById("cursor"); //Getting the cursor
+var body =  document.querySelector("body"); //Get the body element
+
+//Functions for showing and hiding the cursor
+//They are referenced the 
+ function showCursor(e) { //Function to show/hide the cursor
+    if(cursor.classList.contains('cursorHidden')) {
+        cursor.classList.remove('cursorHidden');
+    }
+    cursor.classList.add('cursorVisible');
+}
+
+function hideCursor(e) {    if(cursor.classList.contains('cursorVisible')) {
+    cursor.classList.remove('cursorVisible');
+    }
+    cursor.classList.add('cursorHidden');
+}
+
+function mousemove(e) { //Function to correctly position the cursor
+    showCursor(); //Toggle show/hide
+  
+    var cursorWidth = cursor.offsetWidth * 0.5;
+    var cursorHeight = cursor.offsetHeight * 0.5;
+
+    var cursorX = e.clientX - cursorWidth; //x-coordinate
+    var cursorY = e.clientY - cursorHeight; //y-coordinate
+    var cursorPos = `translate(${cursorX}px, ${cursorY}px)`;
+    cursor.style.transform = cursorPos;
+}
+
+//Eventlisteners
+window.addEventListener('mousemove', mousemove); //Attach an event listener
+body.addEventListener('mouseleave', hideCursor);
+
+
+// ---- App interactions ----
+
+// Open Safari app by toggling .hidden class
+document.querySelector('.appSafari').addEventListener('click', openSafari);
+function openSafari() {
+    document.querySelector('#safari').classList.toggle('hidden');
+    document.querySelector('#messenger').classList.add('hidden');
+    document.querySelector('#music').classList.add('hidden');
+    document.querySelector('#mail').classList.add('hidden');
+    document.querySelector('#settings').classList.add('hidden');
+}
+
+// Open Messenger app
+document.querySelector('.appMessenger').addEventListener('click', openMessenger);
+function openMessenger() {
+    document.querySelector('#messenger').classList.toggle('hidden');
+    document.querySelector('#safari').classList.add('hidden');
+    document.querySelector('#music').classList.add('hidden');
+    document.querySelector('#mail').classList.add('hidden');
+    document.querySelector('#settings').classList.add('hidden');
+}
+
+// Open Music app
+document.querySelector('.appMusic').addEventListener('click', openMusic);
+function openMusic() {
+    document.querySelector('#music').classList.toggle('hidden');
+    document.querySelector('#safari').classList.add('hidden');
+    document.querySelector('#messenger').classList.add('hidden');
+    document.querySelector('#mail').classList.add('hidden');
+    document.querySelector('#settings').classList.add('hidden');
+}
+
+// Open Mail app
+document.querySelector('.appMail').addEventListener('click', openMail);
+function openMail() {
+    document.querySelector('#mail').classList.toggle('hidden');
+    document.querySelector('#safari').classList.add('hidden');
+    document.querySelector('#music').classList.add('hidden');
+    document.querySelector('#messenger').classList.add('hidden');
+    document.querySelector('#settings').classList.add('hidden');
+}
+
+// Open Settings app
+document.querySelector('.appSettings').addEventListener('click', openMail);
+function openMail() {
+    document.querySelector('#settings').classList.toggle('hidden');
+    document.querySelector('#safari').classList.add('hidden');
+    document.querySelector('#music').classList.add('hidden');
+    document.querySelector('#mail').classList.add('hidden');
+    document.querySelector('#messenger').classList.add('hidden');
+}
